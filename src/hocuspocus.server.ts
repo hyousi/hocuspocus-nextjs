cadd0e1d3cb23b5ce225808be2d75af1d105dbd3
zeked <ze.dang@outlook.com>
2024-07-25 13:11:44 +0800

    chore: Add Prisma configuration and Hocuspocus server/client setup

---
+++ src/hocuspocus.server.ts
@@ -0,0 +1,15 @@
+import { SQLite } from "@hocuspocus/extension-sqlite";
+import { Hocuspocus } from "@hocuspocus/server";
+
+// Configure the server …
+const server = new Hocuspocus({
+	port: 1234,
+	extensions: [
+		new SQLite({
+			database: "prisma/db.sqlite",
+		}),
+	],
+});
+
+// … and run it!
+server.listen();
